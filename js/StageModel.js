309f515f2d180af773499d7cb12ca6c5ebcc1cd7
kcarollee <kcarollee31693@gmail.com>
2023-05-21 22:24:09 +0900

    Separated VolumetricSculpture

--- js/StageModel.js
+++ js/StageModel.js
@@ -1,182 +1,7 @@
 import * as THREE from "three";
 import WFCFloorMesh from "./WFCFloorMesh.js";
 import WFC3D from "./WFC3D.js";
-
-
-class VolumetricSculpture {
-    constructor(cameraPos, meshPos){
-        this.size = 32;
-        this.data = new Uint8Array(this.size * this.size * this.size);
-        this.init3DTexture();
-        this.texture = new THREE.Data3DTexture(this.data, this.size, this.size, this.size);
-        let vertexShader = VolumetricSculpture.vertexShader;
-        let fragmentShader = VolumetricSculpture.fragmentShader;
-        this.material = new THREE.RawShaderMaterial({
-            glslVersion: THREE.GLSL3,
-            uniforms:{
-                map: {value: this.texture},
-                cameraPos: {value: new THREE.Vector3(0, 0, 0)},
-                threshold: {value: 0.25},
-                steps: {value: 100}
-            },
-            vertexShader,
-            fragmentShader
-        });
-        
-        this.geometry = new THREE.BoxGeometry(1, 1, 1);
-        this.mesh = new THREE.Mesh(this.geometry, this.material);
-        //this.mesh.position.copy(meshPos);
-    }
-
-    map(value,  min1,  max1,  min2,  max2) {
-        return min2 + (value - min1) * (max2 - min2) / (max1 - min1);
-    }
-
-    init3DTexture(){
-        let vector = new THREE.Vector3();
-        let index = 0;
-        for (let z = 0; z < this.size; z++){
-    	    for (let y = 0; y < this.size; y++){
-    	        for (let x = 0; x < this.size; x++){
-    	           this.data[index++] = this.map(Math.random(), 0, 1, 0, 256);
-                }
-    	    }
-    	}
-        
-    }
-
-    update3DTexture(){
-
-    }
-
-    addToGroup(group){
-        if (group.type === 'Group'){
-            group.add(this.mesh);
-            console.log("ADDED");
-        }
-    }
-}
-
-
-VolumetricSculpture.vertexShader = `
-    in vec3 position;
-    uniform mat4 modelMatrix;
-    uniform mat4 modelViewMatrix;
-    uniform mat4 projectionMatrix;
-    uniform vec3 cameraPos;
-
-    // view origin (camera position) and view direction must be sent to the fragment shader.
-    out vec3 vOrigin;
-    out vec3 vDirection;
-    void main() {
-        vec4 mvPosition = modelViewMatrix * vec4( position, 1.0 );
-        vOrigin = vec3( inverse( modelMatrix ) * vec4( cameraPos, 1.0 ) ).xyz;
-        vDirection = position - vOrigin;
-        gl_Position = projectionMatrix * mvPosition;
-    }
-`;
-
-VolumetricSculpture.fragmentShader = `
-    precision highp float;
-    precision highp sampler3D;
-    uniform mat4 modelViewMatrix;
-    uniform mat4 projectionMatrix;
-
-    // sent from the vert shader with appropriate matrix multiplications
-    in vec3 vOrigin;
-    in vec3 vDirection;
-
-    out vec4 color;
-
-    // 3d texture sent from 'const texture = new THREE.DataTexture3D(data, size, size, size);''
-    uniform sampler3D map;
-
-
-    uniform float threshold;
-    uniform float steps;
-
-    vec2 hitBox( vec3 orig, vec3 dir ) {
-        const float range = 0.5;
-        const vec3 box_min = vec3( -range ); // lower bound
-        const vec3 box_max = vec3(range); // upper bound
-        
-        vec3 inv_dir = 1.0 / dir; // inverse of ray direction
-        
-        vec3 tmin_tmp = ( box_min - orig ) * inv_dir;
-        vec3 tmax_tmp = ( box_max - orig ) * inv_dir;
-        
-        vec3 tmin = min( tmin_tmp, tmax_tmp );
-        vec3 tmax = max( tmin_tmp, tmax_tmp );
-        
-        float t0 = max( tmin.x, max( tmin.y, tmin.z ) );
-        float t1 = min( tmax.x, min( tmax.y, tmax.z ) );
-        
-        return vec2( t0, t1 );
-    }
-
-    // return the r value of the texture at position p
-    float sample1( vec3 p ) {
-        return texture( map, p ).r;
-    }
-
-    #define epsilon .00001
-
-    vec3 normal( vec3 coord ) {
-        if ( coord.x < epsilon ) return vec3( 1.0, 0.0, 0.0 );
-        if ( coord.y < epsilon ) return vec3( 0.0, 1.0, 0.0 );
-        if ( coord.z < epsilon ) return vec3( 0.0, 0.0, 1.0 );
-        
-        if ( coord.x > 1.0 - epsilon ) return vec3( - 1.0, 0.0, 0.0 );
-        if ( coord.y > 1.0 - epsilon ) return vec3( 0.0, - 1.0, 0.0 );
-        if ( coord.z > 1.0 - epsilon ) return vec3( 0.0, 0.0, - 1.0 );
-        
-        float step = 0.01;
-        
-        float x = sample1( coord + vec3( - step, 0.0, 0.0 ) ) - sample1( coord + vec3( step, 0.0, 0.0 ) );
-        float y = sample1( coord + vec3( 0.0, - step, 0.0 ) ) - sample1( coord + vec3( 0.0, step, 0.0 ) );
-        float z = sample1( coord + vec3( 0.0, 0.0, - step ) ) - sample1( coord + vec3( 0.0, 0.0, step ) );
-        
-        return normalize( vec3( x, y, z ) );
-    }
-    void main(){
-        vec3 rayDir = normalize( vDirection );
-        
-        vec2 bounds = hitBox( vOrigin, rayDir );
-        
-        if ( bounds.x > bounds.y ) discard;
-        
-        bounds.x = max( bounds.x, 0.0 );
-        
-        vec3 p = vOrigin + bounds.x * rayDir;
-        
-        vec3 inc = 1.0 / abs( rayDir );
-        
-        float delta = min( inc.x, min( inc.y, inc.z ) );
-        
-        delta /= steps;
-        
-        for ( float t = bounds.x; t < bounds.y; t += delta ) {
-            
-            float d = sample1( p + 0.5 );
-            if ( d > threshold ) {
-                //color.rgb = normal( p + 0.5 ) * 0.5 + ( p * 1.5 + 0.25 );
-                color.rgb = normal(p + 0.5) + p;
-
-                color.a = 1.;
-                break;
-            }
-            p += rayDir * delta;
-
-        }
-
-        float gs = (color.r + color.g + color.b) / 2.0;
-
-        color.rgb = 1.0 - normal(p + 0.5);
-        color.rgb *= 0.75;
-        if ( color.a == 0.0 ) discard;
-    }
-`;
-
+import VolumetricSculpture from "./VolumetricSculpture.js";
 
 export default class StageModel {
     constructor(difficulty) {
@@ -306,6 +131,7 @@ export default class StageModel {
             unlocked: false,
         };
 
+        this.volumeModels = [];
         
     }
 
@@ -486,65 +312,73 @@ export default class StageModel {
                 
             }
             */
+            
+            
+            
             // structure 5: Noise structure
-            
-            
-
-            {
+            // {
                 
                 
 
-                noise.seed(Math.random())
-                console.log(noise.simplex3(1.2, 1.4, 2.4));
+            //     noise.seed(Math.random())
+            //     console.log(noise.simplex3(1.2, 1.4, 2.4));
 
-                let totalDim = Math.min(sizeX, sizeZ);
-                let dimScale = 0.9;
-                totalDim *= dimScale;
+            //     let totalDim = Math.min(sizeX, sizeZ);
+            //     let dimScale = 0.9;
+            //     totalDim *= dimScale;
 
-                let divNum = Math.floor(this.map(Math.random(), 0, 1, 5, 8));
-                let cubeDim = totalDim / divNum;
+            //     let divNum = Math.floor(this.map(Math.random(), 0, 1, 5, 8));
+            //     let cubeDim = totalDim / divNum;
                 
-                let startX = buildingPosX - sizeX * 0.5 + cubeDim;
-                let startZ = buildingPosZ - sizeZ * 0.5 + cubeDim;
-                let startY = cubeDim;
+            //     let startX = buildingPosX - sizeX * 0.5 + cubeDim;
+            //     let startZ = buildingPosZ - sizeZ * 0.5 + cubeDim;
+            //     let startY = cubeDim;
 
-                let cubeGeom = new THREE.BoxGeometry(1, 1, 1);
+            //     let cubeGeom = new THREE.BoxGeometry(1, 1, 1);
 
-                let dummy = new THREE.Object3D();
-                let cubeGroup = new THREE.Group();
-                let cubeInstancedMesh = new THREE.InstancedMesh(cubeGeom, randomCubeMeshMat, divNum * divNum * divNum);
-                let index = 0;
-                for (let yNum = 0; yNum < divNum; yNum++){
-                    for (let zNum = 0; zNum < divNum; zNum++){
-                        for (let xNum = 0; xNum < divNum; xNum++){
-                            let cubePosX = startX + cubeDim * xNum;
-                            let cubePosY = startY + cubeDim * yNum;
-                            let cubePosZ = startZ + cubeDim * zNum;
-                            //let cubeMesh = new THREE.Mesh(cubeGeom, randomCubeMeshMat);
+            //     let dummy = new THREE.Object3D();
+            //     let cubeGroup = new THREE.Group();
+            //     let cubeInstancedMesh = new THREE.InstancedMesh(cubeGeom, randomCubeMeshMat, divNum * divNum * divNum);
+            //     let index = 0;
+            //     for (let yNum = 0; yNum < divNum; yNum++){
+            //         for (let zNum = 0; zNum < divNum; zNum++){
+            //             for (let xNum = 0; xNum < divNum; xNum++){
+            //                 let cubePosX = startX + cubeDim * xNum;
+            //                 let cubePosY = startY + cubeDim * yNum;
+            //                 let cubePosZ = startZ + cubeDim * zNum;
+            //                 //let cubeMesh = new THREE.Mesh(cubeGeom, randomCubeMeshMat);
                             
-                            let noiseCoef = 2.0;
-                            let scale = noise.simplex3(cubePosX * noiseCoef, cubePosY * noiseCoef, cubePosZ * noiseCoef);
-                            //console.log(scale);
-                            scale = this.map(scale, -1, 1, 0, 0.25);
-                            /*
-                            cubeMesh.scale.set(scale, scale, scale);
-                            cubeMesh.position.set(cubePosX, cubePosY, cubePosZ);
-                            cubeGroup.add(cubeMesh);
-                            */
+            //                 let noiseCoef = 2.0;
+            //                 let scale = noise.simplex3(cubePosX * noiseCoef, cubePosY * noiseCoef, cubePosZ * noiseCoef);
+            //                 //console.log(scale);
+            //                 scale = this.map(scale, -1, 1, 0, 0.25);
+            //                 /*
+            //                 cubeMesh.scale.set(scale, scale, scale);
+            //                 cubeMesh.position.set(cubePosX, cubePosY, cubePosZ);
+            //                 cubeGroup.add(cubeMesh);
+            //                 */
 
-                            dummy.position.set(cubePosX, cubePosY, cubePosZ);
-                            dummy.scale.set(scale, scale, scale);
-                            dummy.updateMatrix();
-                            cubeInstancedMesh.setMatrixAt(index++, dummy.matrix);
-                        }
-                    }
-                }
-                this.meshGroup.add(cubeInstancedMesh);
+            //                 dummy.position.set(cubePosX, cubePosY, cubePosZ);
+            //                 dummy.scale.set(scale, scale, scale);
+            //                 dummy.updateMatrix();
+            //                 cubeInstancedMesh.setMatrixAt(index++, dummy.matrix);
+            //             }
+            //         }
+            //     }
+            //     this.meshGroup.add(cubeInstancedMesh);
+            // }
 
-                
-                
-                
+            // structure 6: volumetric model 
+            {
+                let tempVol = new VolumetricSculpture(
+                    this.playerPosition,
+                    new THREE.Vector3(buildingPosX, 1, buildingPosZ),
+                    new THREE.Vector3(sizeX, 1, sizeZ)
+                );
+                tempVol.addToGroup(this.meshGroup);
             }
+
+            console.log(this.meshGroup);
         }
         
         //console.log(playerPosX, playerPosZ, newBuildingTransformArr);
